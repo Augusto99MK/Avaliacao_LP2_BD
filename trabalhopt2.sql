b6d9a22525ae97a4c901811acc77992b95bf4357
Augusto99MK <carlosaugustox6@gmail.com>
2023-06-05 20:05:01 -0400

    Imagem Adicionada

---
+++ trabalhopt2.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27/05/2023 às 21:00
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `trabalhopt2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nomeCliente` varchar(255) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `emailCliente` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nomeCliente`, `cpf`, `emailCliente`) VALUES
+(1, 'carlos', '1236987456', 'carlosteste@gmail.com.br');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `softwarelicenca`
+--
+
+CREATE TABLE `softwarelicenca` (
+  `id` int(11) NOT NULL,
+  `nomeSoftware` varchar(255) NOT NULL,
+  `cliente` int(11) DEFAULT NULL,
+  `emailCliente` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `softwarelicenca`
+--
+
+INSERT INTO `softwarelicenca` (`id`, `nomeSoftware`, `cliente`, `emailCliente`) VALUES
+(1, 'Software Novo', 1, 'carlosteste@gmail.com');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `softwarelicenca`
+--
+ALTER TABLE `softwarelicenca`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cliente` (`cliente`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `softwarelicenca`
+--
+ALTER TABLE `softwarelicenca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `softwarelicenca`
+--
+ALTER TABLE `softwarelicenca`
+  ADD CONSTRAINT `fk_cliente` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `softwarelicenca_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
